c3240d247db178c5b468ee4c0ad0d355d7244ef9
LeThanhVu1995 <lethanhvu20015@gmail.com>
2021-10-30 15:49:38 +0700

    try it to fix bug

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -1,6 +1,6 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-export default async (req, res) => {
+export default async function handler(req, res) {
   if (req.method === "POST") {
     const { items, email } = req.body;
 
@@ -35,4 +35,4 @@ export default async (req, res) => {
 
     res.status(200).json({ id: session.id });
   }
-};
+}
